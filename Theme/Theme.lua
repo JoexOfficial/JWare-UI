d48f0bdbe6a4d6a68523dd40afdff3d8d9b16e19
JoexOfficial <joex.360hz@outlook.com>
2025-09-03 02:10:38 +0200

    Create Theme.lua

---
+++ Theme/Theme.lua
@@ -0,0 +1,7 @@
+return {
+    JWare = {
+        MainColor = Color3.fromRGB(70, 7, 100),
+        OutlineColor = Color3.fromRGB(16, 16, 16),
+        BackgroundColor = Color3.fromRGB(27, 27, 27)
+    },
+}
